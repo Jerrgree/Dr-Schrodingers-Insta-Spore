1359d68273e19f5083ffdb75ef9fbb3fcf2a9139
Jerrgree <jeremyrgreenburg@gmail.com>
2018-01-18 21:52:59 -0600

    Added the main script.
    Only further change needed is the actual room log of schrodinger
    scratching the player

---
+++ Sporify.js
@@ -0,0 +1,52 @@
+roomLogs = $.grep(Array.from(document.querySelectorAll('div')), function(e){return $.inArray("cdChatLine", e.classList) !== -1})
+// Seed our pseudo page with the initial page
+element = document.createElement('div');
+element.innerHTML = $('html').html();
+// While we haven't hurt ourselves from schrody
+//while (!roomLogs[0].innerText.trim().match("^SchrodingerScratchLog"))
+for (var i = 0; i < 10; i++)
+{
+	// If we can drop something, do that
+	dropCat = Array.from(element.querySelectorAll('a')).find(el => ~el.href.indexOf("DROP_OBJECT"))
+	if (!!dropCat)
+	{
+		console.log("dropping");
+		$.ajax({
+			async: false,
+			type: 'GET',
+			url: dropCat.href,
+			dataType: "text",
+			success: function(t)
+			{
+				//console.log(t);
+				element.innerHTML = t;
+			}
+		});
+
+	}
+	// If we haven't dropped this turn, pick up the cat
+	getCat = Array.from(element.querySelectorAll('a')).find(el => ~el.href.indexOf("PICK_CAT"))
+	if (getCat !== undefined && dropCat === undefined)
+	{
+		console.log("picking");
+		$.ajax({
+			async: false,
+			type: 'GET',
+			url: getCat.href,
+			dataType: "text",
+			success: function(t)
+			{
+				//console.log(t);
+				element.innerHTML = t;
+			}
+		});
+	}
+	// Oops there's no cat in this room
+	else if (getCat === undefined && dropCat === undefined)
+	{
+		throw new Error("There is no cat in this room.");
+	}
+	//Update the room logs
+	roomLogs = $.grep(Array.from(element.querySelectorAll('div')), function(e){return $.inArray("cdChatLine", e.classList) !== -1})
+}
+location.reload();
